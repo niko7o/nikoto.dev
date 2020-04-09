56cfa88503330f699cb7291eda3e0e13bceeb704
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 01:53:51 +0200

    more progress

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,9 +6,21 @@ const CURRENT_COMPANY = 'Spotahome';
 
 const Bio = () => (
   <div className="bio">
-    <p className="bio__text">Heya! I'm</p>
-    <p className="bio__name">nikoto</p>
-    <p className="bio__occupation">Software Engineer @ {CURRENT_COMPANY}</p>
+    <div className="bio__information-basic">
+      <p className="bio__name">Heya! I'm <span className="bio__name--heavy">nikoto</span></p>
+      <p className="bio__occupation">Software Engineer<br />@ {CURRENT_COMPANY}</p>
+    </div>
+
+    <div className="bio__information-socials">
+      <a href="https://github.com/nikotomad" target="_blank" className="bio__information-socials-link">
+        <img className="bio__information-icon" src="./images/icon-github.svg" width="40"/>
+        Github
+      </a>
+      <a href="https://www.linkedin.com/in/nikolaasverlee/" target="_blank" className="bio__information-socials-link">
+        <img className="bio__information-icon" src="./images/icon-linkedin.webp" width="40"/>
+        Linkedin
+      </a>
+    </div>
   </div>
 )
 
