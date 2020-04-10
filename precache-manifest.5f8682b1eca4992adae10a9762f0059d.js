b39968a98d631e5c1a02d26a6ed75a846c6ed95e
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 02:24:20 +0200

    Updates

---
+++ precache-manifest.5f8682b1eca4992adae10a9762f0059d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1aeda7405affc1b160e131fda8b92a",
+    "url": "/nikoto.dev/index.html"
+  },
+  {
+    "revision": "ee176f0af5c1737eb1d5",
+    "url": "/nikoto.dev/static/css/main.fa96c765.chunk.css"
+  },
+  {
+    "revision": "f1182f09c121c0f58549",
+    "url": "/nikoto.dev/static/js/2.41e2711b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nikoto.dev/static/js/2.41e2711b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee176f0af5c1737eb1d5",
+    "url": "/nikoto.dev/static/js/main.e9555538.chunk.js"
+  },
+  {
+    "revision": "bddc5a513eb7908e7c41",
+    "url": "/nikoto.dev/static/js/runtime-main.cbd98ce8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.133a95662baba28e8cf366d16c19b657.js"
+  "/nikoto.dev/precache-manifest.5f8682b1eca4992adae10a9762f0059d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nikoto.dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
