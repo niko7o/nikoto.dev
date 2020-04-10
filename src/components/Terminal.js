1e5e9b3c01602f44997291f9f6990c46317aa67b
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 18:13:11 +0200

    qol fixes to terminal to clear warnings

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-mixed-operators */
 import React, { useState } from 'react';
 
 import { commands } from '../utils/terminal';
+
+import { PanelConsumer } from '../contexts/panel-context';
 
 import './Terminal.scss';
 
@@ -10,9 +13,11 @@ const Terminal = () => {
   const [commandHistory, setCommandHistory] = useState([]);
   const [command, setCommand] = useState('');
 
-  const handleInputChange = e => setCommand(e.target.value);
+  const handleInputChange = e => 
+    setCommand(e.target.value);
 
-  const clearCommandEmitter = () => setCommand('');
+  const clearCommandEmitter = () => 
+    setCommand('');
 
   const handleTerminalKeyPressed = ({ key }) => {
     if (key === 'Enter') {
@@ -22,26 +27,44 @@ const Terminal = () => {
   }
 
   return (
-    <div className="terminal">
-      <div className="terminal__navbar">
-        <span className="terminal__navbar-close" />
-      </div>
-      <div className="terminal__screen">
-        <p className="terminal__screen-hint"></p>
-        <div className="terminal__screen-results">
-          {commandHistory.map((cmd, i) => (
-            <div className="terminal__screen-row" key={`${cmd}${i}`}>
-              <p className="terminal__command">{`${PATH_PREFIX} → ${cmd}`}</p>
-              <p className="terminal__result">{commands[cmd] && commands[cmd].result || 'Command not found. Type "help" for more info.'}</p>
+    <PanelConsumer>
+      {panelContext => (
+        <div className="terminal">
+          <div className="terminal__navbar">
+            <span className="terminal__navbar-close" />
+          </div>
+          <div className="terminal__screen">
+            <p className="terminal__screen-hint"></p>
+            <div className="terminal__screen-results">
+              {commandHistory.map((cmd, i) => (
+                <div className="terminal__screen-row" key={`${cmd}${i}`}>
+                  <p className="terminal__command">
+                    {`${PATH_PREFIX} → ${cmd}`}
+                  </p>
+                  <p className="terminal__result">
+                    {
+                      commands[cmd] && commands[cmd].result 
+                      || 'Command not found. Type "help" for more info.'
+                    }
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+          <div className="terminal__actions" onKeyDown={handleTerminalKeyPressed}>
+              <span className="terminal__actions-symbol">{`$ ${PATH_PREFIX}`}</span>
+              <input 
+                autoFocus 
+                type="text" 
+                className="terminal__actions-command" 
+                value={command}
+                onChange={handleInputChange}
+                tabIndex="0"
+              />
+          </div>
         </div>
-      </div>
-      <div className="terminal__actions" onKeyDown={handleTerminalKeyPressed}>
-          <span className="terminal__actions-symbol">{`$ ${PATH_PREFIX}`}</span>
-        <input tabIndex="0" autoFocus className="terminal__actions-command" type="text" value={command} onChange={handleInputChange}/>
-      </div>
-    </div>
+      )}
+    </PanelConsumer>
   )
 }
 
