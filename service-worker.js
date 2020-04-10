24bcabbd2c8309327452d3461eb6ac4956f6bbf7
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 21:26:45 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a46868fdd0e6d500ca1a7bf3aead941.js"
+  "/precache-manifest.03b126ffa880cb9c468e5ec61bd7b882.js"
 );
 
 self.addEventListener('message', (event) => {
