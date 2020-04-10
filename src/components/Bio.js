e4694d565670fcf60ec0f071f5dcdffad10af63b
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 16:47:54 +0200

    QOL changes + responsive

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,11 +5,11 @@ import './Bio.scss';
 const Bio = () => (
   <div className="bio">
     <div className="bio__information-basic">
-      <p className="bio__name">Heya! 👋 I'm <span className="bio__name--heavy">nikoto.</span></p>
-      <p className="bio__occupation">Software Engineer<br />@ Spotahome <img className="bio__spotahome" src="./images/spotahome.jpg" /></p>
+      <p className="bio__name">Heya! <span role="img" className="bio__waving-hand" aria-label="wave">👋</span> I'm <span className="bio__name--heavy">nikoto.</span></p>
+      <p className="bio__occupation">Software Engineer<br />@ Spotahome <img className="bio__spotahome" src="./images/spotahome.jpg" alt="spotahome logo" /></p>
     </div>
 
-    <code className="bio__teaser">Want to know me better? Use the terminal. Start by typing "help" and pressing enter.</code>
+    <code className="bio__teaser">Want to know me better? Use the terminal. <br />Start by typing "help" and pressing enter.</code>
 
     <div className="bio__information-socials">
       <a href="https://github.com/nikotomad" target="_blank" className="bio__information-socials-link">
