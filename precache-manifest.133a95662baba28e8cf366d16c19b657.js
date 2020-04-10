bb806265c211731cebc45eaf65bbce0bb18a8308
Nikolaas Verlee <nikotomad@gmail.com>
2020-04-10 02:17:17 +0200

    Updates

---
+++ precache-manifest.133a95662baba28e8cf366d16c19b657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea0ce6d24f64fd076807cdbdf2d9ed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c890c1cff6449e0b99",
+    "url": "/static/css/main.fa96c765.chunk.css"
+  },
+  {
+    "revision": "9444386ca3808607f45c",
+    "url": "/static/js/2.6f86efc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f86efc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c890c1cff6449e0b99",
+    "url": "/static/js/main.c9ba7bf2.chunk.js"
+  },
+  {
+    "revision": "b40c2e1e536c64dcb738",
+    "url": "/static/js/runtime-main.8002f4b3.js"
+  }
+]);
